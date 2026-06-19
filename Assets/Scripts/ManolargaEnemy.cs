a79a0ca31d3ea7068c0de75d94a96763df4db9be
itzkyoto <minatonamicase531@gmail.com>
2026-06-19 13:33:09 -0500

    switch funcional del ManoLarga

--- Assets/Scripts/ManolargaEnemy.cs
+++ Assets/Scripts/ManolargaEnemy.cs
@@ -19,11 +19,10 @@ public class ManolargaEnemy : MonoBehaviour
     public float Speed = 5;
     GameObject target;
     public float radiusattack = 1;
-    public float radiusMovement = 5;
-    public bool AttackMode = false;
+    public float radiusMovement = 2.5f;
+    public float RadiusFlee = 5f;
+    
 
-
-    public float timeToChangeMode = 5;
     public float currentTime = 0;
 
     public bool IsAbleToAttack = true;
@@ -54,11 +53,16 @@ public class ManolargaEnemy : MonoBehaviour
         switch (state)
         {
             case EnemyState.None:
+                state = EnemyState.Idle;
                 break;
 
             case EnemyState.Idle:
                 {
-                    if (Vector3.Distance(targetPos, myPos) < radiusMovement)
+
+                    if (Vector3.Distance(targetPos, myPos) <= RadiusFlee)
+                        state = EnemyState.Flee;
+
+                    if (Vector3.Distance(targetPos, myPos) <= radiusMovement)
                         state = EnemyState.Chase;
                 }
                 break;
@@ -70,7 +74,7 @@ public class ManolargaEnemy : MonoBehaviour
 
                     
                     if (Vector3.Distance(targetPos, myPos) > radiusMovement)
-                        state = EnemyState.Idle;
+                        state = EnemyState.Flee;
                     
                     if (Vector3.Distance(targetPos, myPos) < radiusattack)
                         state = EnemyState.Attack;
@@ -79,31 +83,50 @@ public class ManolargaEnemy : MonoBehaviour
 
             case EnemyState.Flee:
                 {
+                   if (Vector3.Distance(targetPos, myPos) < RadiusFlee)
+                    {
+                        Vector3 direction = (targetPos - myPos).normalized;
+                        transform.position -= direction * Speed * Time.deltaTime;
+                    }
 
+                    if (Vector3.Distance(targetPos, myPos) > RadiusFlee)
+                    {
+                        state = EnemyState.Idle;
+                    }
 
+                    if (Vector3.Distance(targetPos, myPos) < radiusMovement)
+                    {
+                        state = EnemyState.Chase;
+                    }
+                        
+                    if (Vector3.Distance(targetPos,myPos) < RadiusFlee && Vector3.Distance(targetPos,myPos) > radiusMovement)
+                    {
+                        state = EnemyState.Attack;
+                    }
                 }
                 break;
 
             case EnemyState.Attack:
                 {
-                    if (IsAbleToAttack)
-                    {
+                    if (Vector3.Distance(targetPos, myPos) < radiusattack)
+                    { 
+                        if (IsAbleToAttack)
+                             {
                         
-                        Debug.Log("Atacando");
-                        target.GetComponent<Player>().Health -= damage;
-                        IsAbleToAttack = false;
+                                 Debug.Log("Atacando");
+                                 target.GetComponent<Player>().Health -= damage;
+                                IsAbleToAttack = false;
+                             }
+                            currentTime += Time.deltaTime;
+                         if (currentTime >= MaxTime)
+                            {
+                                 IsAbleToAttack = true;
+
+                                    currentTime = 0;
+                             }
                     }
-                    currentTime += Time.deltaTime;
-                    if (currentTime >= MaxTime)
-                    {
-                        IsAbleToAttack = true;
-
-                        currentTime = 0;
-                    }
-                    
 
 
-                    
                     SpecialabilityReset += Time.deltaTime;
                     if (SpecialabilityReset >= MaxAbilityTime)
                     {
@@ -112,16 +135,27 @@ public class ManolargaEnemy : MonoBehaviour
 
                         SpecialabilityReset = 0;
                     }
-                    if (radiusattack == 5)
+
+                    if ( IsAbleToUseSpecialAbility == true)
                     {
+                        SpecialabilityReset += Time.deltaTime;
+                        if (SpecialabilityReset >= MaxAbilityTime)
+                        {
+
+                            IsAbleToUseSpecialAbility = true;
+
+                            SpecialabilityReset = 0;
+                        }
+
                             target.GetComponent<Player>().Health -= SpecialAbilityDamage;
                             IsAbleToUseSpecialAbility = false;
                             Debug.Log("toma toma te pego piu piu");
                         
+                                
                     }
 
                     if (Vector3.Distance(targetPos, myPos) > radiusattack)
-                        state = EnemyState.Chase;
+                        state = EnemyState.Flee;
                 }
                 break;
 
@@ -134,110 +168,5 @@ public class ManolargaEnemy : MonoBehaviour
 
     }
 
-    public void ChangeMode()
-    {
-        currentTime += Time.deltaTime;
-        if (currentTime > timeToChangeMode)
-        {
-            AttackMode = !AttackMode;
-            currentTime = 0;
-        }
-        if (AttackMode == false)
-        {
-            FollowTarget(radiusMovement);
-            if (!IsAbleToAttack)
-                TimeToDoSet();
-
-
-        }
-        else
-        {
-            AvoidTarget(radiusattack);
-            if (!IsAbleToUseSpecialAbility)
-                TimeToDoSpeciaAbility();
-        }
-
-    }
-
-    public void FollowTarget(float radiusMovement)
-    {
-        if (target == null)
-        {
-            return;
-        }
-        radiusMovement = 10;
-        Vector3 direction = (target.transform.position - transform.position).normalized;
-        if (Vector3.Distance(transform.position, target.transform.position) < radiusMovement)
-        {
-            if (Vector3.Distance(transform.position, target.transform.position) < radiusattack)
-            {
-                Debug.Log("ven maldito");
-                if (IsAbleToAttack)
-                {
-                    target.GetComponent<Player>().Health -= damage;
-                    IsAbleToAttack = false;
-                    Debug.Log("toma toma");
-                }
-
-            }
-            else
-            {
-                transform.position += direction * Speed * Time.deltaTime;
-            }
-        }
-
-    }
-    public void AvoidTarget(float radiusattack)
-    {
-        if (target == null)
-        {
-            return;
-        }
-
-        Vector3 dir = (target.transform.position - transform.position).normalized;
-
-        if (Vector3.Distance(transform.position, target.transform.position) < radiusMovement)
-        {
-            transform.position += -dir * Speed * Time.deltaTime;
-       
-            
-        }
-        else
-        {
-            if (radiusattack == 5)
-            {
-                if (IsAbleToUseSpecialAbility)
-                {
-                    target.GetComponent<Player>().Health -= SpecialAbilityDamage;
-                    IsAbleToUseSpecialAbility = false;
-                    Debug.Log("toma toma te pego piu piu");
-                }
-            }
-           
-        }
-
-    }
-    public void TimeToDoSet()
-    {
-        ResetTime += Time.deltaTime;
-        if (ResetTime >= MaxTime)
-        {
-
-            IsAbleToAttack = true;
-
-            ResetTime = 0;
-        }
-    }
-
-    public void TimeToDoSpeciaAbility()
-    {
-        SpecialabilityReset += Time.deltaTime;
-        if (SpecialabilityReset >= MaxAbilityTime)
-        {
-
-            IsAbleToUseSpecialAbility = true;
-
-            SpecialabilityReset = 0;
-        }
-    }
+    
 }
